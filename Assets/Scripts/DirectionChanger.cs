d7430184d7df36cf595ad8f6c49d38834d305c01
Yusuf Tulek <enes_tulek@live.com>
2022-11-07 17:47:02 +0300

    add direction changer

---
+++ Assets/Scripts/DirectionChanger.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class DirectionChanger : MonoBehaviour
+{
+	[SerializeField] InputChannel _inputChannel;
+	[SerializeField] Transform _visualsToRotate;
+	bool _isFacingRight;
+
+	void OnEnable()
+	{
+		_inputChannel.Moving += InputChannelOnMoving;
+	}
+
+	void OnDisable()
+	{
+		_inputChannel.Moving -= InputChannelOnMoving;
+	}
+	
+	void InputChannelOnMoving(Vector2 moveVector)
+	{
+		if (_isFacingRight && moveVector.x < 0f)
+		{
+			_isFacingRight = false;
+			_visualsToRotate.eulerAngles = Vector3.zero;
+		}
+		else if (!_isFacingRight && moveVector.x > 0f)
+		{
+			_isFacingRight = true;
+			_visualsToRotate.eulerAngles = new Vector3(0f, 180f, 0f);
+		}
+
+	}
+}
