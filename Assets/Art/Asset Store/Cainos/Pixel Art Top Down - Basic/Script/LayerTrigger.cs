3c235050c056bff33e45d14b5dbac1e28780bf04
Yusuf Tulek <enes_tulek@live.com>
2022-11-07 17:57:48 +0300

    add pixel art top down package

---
+++ Assets/Art/Asset Store/Cainos/Pixel Art Top Down - Basic/Script/LayerTrigger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cainos.PixelArtTopDown_Basic
+{
+    //when object exit the trigger, put it to the assigned layer and sorting layers
+    //used in the stair objects for player to travel between layers
+    public class LayerTrigger : MonoBehaviour
+    {
+        public string layer;
+        public string sortingLayer;
+
+        void OnTriggerExit2D(Collider2D other)
+        {
+            other.gameObject.layer = LayerMask.NameToLayer(layer);
+
+            if (other.gameObject.TryGetComponent(out SpriteRenderer spriteRenderer))
+            {
+                spriteRenderer.sortingLayerName = sortingLayer;
+            }
+            
+            SpriteRenderer[] srs = other.gameObject.GetComponentsInChildren<SpriteRenderer>();
+            foreach ( SpriteRenderer sr in srs)
+            {
+                sr.sortingLayerName = sortingLayer;
+            }
+        }
+
+    }
+}
