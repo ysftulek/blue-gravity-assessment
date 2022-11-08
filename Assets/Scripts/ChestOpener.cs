eb7e98fbfb8f5eaab39dfeb654da86eda2141f2b
Yusuf Tulek <enes_tulek@live.com>
2022-11-08 19:40:04 +0300

    Update ChestOpener.cs

--- Assets/Scripts/ChestOpener.cs
+++ Assets/Scripts/ChestOpener.cs
@@ -1,16 +1,31 @@
 using System;
+using DG.Tweening;
 using UnityEngine;
 
 public class ChestOpener : MonoBehaviour, IInteractable
 {
 	[SerializeField] SpriteRenderer _spriteRenderer;
 	[SerializeField] Sprite _interactedSprite;
+	[SerializeField] SaveableRuntimeIntVariable _money;
+	[SerializeField] int _moneyIncrease;
 
 	bool _isInteracted;
 
 	public void Interact(Interactor interactor, Action interactionEndedCallback)
 	{
-		_spriteRenderer.sprite = _interactedSprite;
+		transform.DOPunchScale(new Vector3(-0.3f,
+					-0.5f,
+					0),
+				0.4f, 0, 0.3f)
+			.OnComplete(() =>
+			{
+				transform.DOPunchScale(new Vector3(0.2f,
+						0.5f,
+						0f),
+					0.2f, 2, 0.8f);
+				_spriteRenderer.sprite = _interactedSprite;
+				_money.RuntimeValue += _moneyIncrease;
+			});
 		
 		interactionEndedCallback?.Invoke();
 	}
